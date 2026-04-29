6680e6b2176c4957382d9d424945d66df6ff9ca3
Sushilkumar <sushilmevada6@gmail.com>
2026-04-29 10:27:51 +0000

    [ADD] SimpleLinearRegressionWithSkLearn

---
+++ LinearRegression/usingSklearn.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the dataset
+from sklearn import datasets
+
+X, y = datasets.load_diabetes(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=0)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(r2_score(y_test,y_pred))
